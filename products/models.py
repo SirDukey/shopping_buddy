65d1b12c723b22d7a6efc24af22e3a681e1aad01
SirDukey <michael_duke@outlook.com>
2023-11-14 14:53:05 +0100

    initial

---
+++ products/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from datetime import datetime
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100, default=None, null=True)
+    store = models.CharField(max_length=100, default=None, null=True)
+    category = models.CharField(max_length=100, default=None, null=True)
+    gluten_free = models.BooleanField(default=False)
+    quantity = models.IntegerField(default=0)
+    price = models.FloatField(default=0)
+    date = models.DateField(default=datetime.today(), null=False)
---
+++ products/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Product
+
+
+def products(request):
+    product_list = Product.objects.all().values()
+    context = {'product_list': product_list}
+    template = loader.get_template('products.html')
+    return HttpResponse(template.render(context, request))
+
+
+def details(request, id):
+    product = Product.objects.get(id=id)
+    template = loader.get_template('product_details.html')
+    context = {'product': product}
+    return HttpResponse(template.render(context, request))
